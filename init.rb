e7da4ebc29f9cd08584e2eb25c8ae30bb9201365
zapic0 <zapico@gmail.com>
2017-02-12 17:16:56 +0100

    Adds author data

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :deployer do
   name 'Deployer plugin'
-  author 'Author name'
+  author 'zapic0'
   description 'This is a plugin for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author_url 'http://github.com/zapic0'
 end
