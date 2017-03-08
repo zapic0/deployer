c1318502aa8fff49122d937c1d5329d8cd877f02
zapic0 <zapico@gmail.com>
2017-03-08 23:48:44 +0100

    Adds params and validation to deploys

--- app/models/deploy.rb
+++ app/models/deploy.rb
@@ -1,7 +1,10 @@
 class Deploy < ActiveRecord::Base
- has_and_belongs_to_many :issues
+  has_and_belongs_to_many :issues
 
- validates :project, :date, :start_time, :estimated_end_time, :release_notes, :affected_projects, :authorization, presence: true
+  validates :project, :date, :start_time, :estimated_end_time, :release_notes, presence: true
 
+  def deploy_params
+    params.require(:deploy).permit(:affected_projects, :release_notes, {:issue_ids => []}, :approved, :date, :start_time, :estimated_end_time, :authorization)
+  end
 
 end
