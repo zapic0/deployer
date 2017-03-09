52aca80fc3aa036924af20d48b90ef0a542a3b04
zapic0 <zapico@gmail.com>
2017-03-09 23:49:54 +0100

    Unify mails subjects

--- app/models/deployer_mailer.rb
+++ app/models/deployer_mailer.rb
@@ -14,13 +14,13 @@ class DeployerMailer < Mailer
 
   def send_successful_end(deploy, project)
     @deploy = deploy
-    mail_subject = "[deploys] {#{project.abbreviation}} Notificación de despliegue #{deploy.deploy_name} - Correcto"
+    mail_subject = "[deploys] {#{project.abbreviation}} Notificación de despliegue #{deploy.deploy_name}"
     mail to: project.customers_deploys_notifications_emails, cc: project.deploys_notifications_emails, subject: mail_subject
   end
 
   def send_rollback(deploy, project)
     @deploy = deploy
-    mail_subject = "[deploys] {#{project.abbreviation}} Notificación de despliegue #{deploy.deploy_name} - Rollback"
+    mail_subject = "[deploys] {#{project.abbreviation}} Notificación de despliegue #{deploy.deploy_name}"
     mail to: project.customers_deploys_notifications_emails, cc: project.deploys_notifications_emails, subject: mail_subject
   end
 
