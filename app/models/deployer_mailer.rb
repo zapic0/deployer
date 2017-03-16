95669e0c83fe3f0f1931de304ebe39b3aac78419
Fernando Fernandez <fernando.zapico@the-cocktail.com>
2017-03-16 17:00:35 +0100

    Send mails to company as bcc

--- app/models/deployer_mailer.rb
+++ app/models/deployer_mailer.rb
@@ -8,20 +8,20 @@ class DeployerMailer < Mailer
   def send_start(deploy, project)
     @deploy = deploy
     mail_subject = "[deploys] {#{project.abbreviation}} Notificación de despliegue #{deploy.deploy_name}"
-    mail to: project.customers_deploys_notifications_emails, cc: project.deploys_notifications_emails, subject: mail_subject
+    mail to: project.customers_deploys_notifications_emails, bcc: project.deploys_notifications_emails, subject: mail_subject
   end
 
 
   def send_successful_end(deploy, project)
     @deploy = deploy
     mail_subject = "[deploys] {#{project.abbreviation}} Notificación de despliegue #{deploy.deploy_name}"
-    mail to: project.customers_deploys_notifications_emails, cc: project.deploys_notifications_emails, subject: mail_subject
+    mail to: project.customers_deploys_notifications_emails, bcc: project.deploys_notifications_emails, subject: mail_subject
   end
 
   def send_rollback(deploy, project)
     @deploy = deploy
     mail_subject = "[deploys] {#{project.abbreviation}} Notificación de despliegue #{deploy.deploy_name}"
-    mail to: project.customers_deploys_notifications_emails, cc: project.deploys_notifications_emails, subject: mail_subject
+    mail to: project.customers_deploys_notifications_emails, bcc: project.deploys_notifications_emails, subject: mail_subject
   end
 
 end
