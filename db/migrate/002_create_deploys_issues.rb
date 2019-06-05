4990d8e037b34715512f25bd8a8a4290c7b92cb0
Tizian Schmidlin <st@cabag.ch>
2019-06-05 10:32:28 +0200

    Set ActiveSupport version

--- db/migrate/002_create_deploys_issues.rb
+++ db/migrate/002_create_deploys_issues.rb
@@ -1,4 +1,4 @@
-class CreateDeploysIssues < ActiveRecord::Migration
+class CreateDeploysIssues < ActiveRecord::Migration[4.2]
   def change
     create_table :deploys_issues do |t|
       t.references :deploy, :issue
